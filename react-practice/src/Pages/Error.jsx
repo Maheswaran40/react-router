d56b7df05e6295ad51f8a0cac43fb35ed0faae36
Maheswaran40 <eswermahes@gmail.com>
2025-12-28 12:49:30 +0530

    error page

--- react-practice/src/Pages/Error.jsx
+++ react-practice/src/Pages/Error.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import back from "../assets/Images/Error.png"
 
 function Error() {
+  const style1 = {
+  fontSize: "150px",
+  backgroundImage: `url(${back})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "100% 100%",
+  backgroundPosition: "center",
+  backgroundClip: "text",
+  color: "transparent"
+}
+
+  var style2={
+    display:"flex",justifyContent:"center",alignItems:"center",height:"100vh"
+  }
   return (
-    <>
+    <div style={style2}>
     
-    <h1>Page Not Found</h1>
-    </>
+    <h1 style={style1}>Page Not Found</h1>
+    </div>
   )
 }
 
